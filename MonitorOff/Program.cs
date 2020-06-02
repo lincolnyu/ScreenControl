1d9bf6abdd1398d3b778d58e0667f171f0bcb86b
Lincoln Yu <quanben@msn.com>
2020-06-03 01:50:06 +1000

    MonitorOff to periodicly turn off monitor.

--- MonitorOff/Program.cs
+++ MonitorOff/Program.cs
@@ -1,11 +1,30 @@
-﻿using static ScreenControlLib.MonitorOff;
+﻿using System;
+using System.Threading;
+using static ScreenControlLib.MonitorOff;
+
 namespace MonitorOff
 {
     class Program
     {
         static void Main(string[] args)
         {
-            SetMonitorInState(MonitorState.MonitorStateOff);
+            var i = Array.IndexOf(args, "-r");
+            if (i >= 0)
+            {
+                if (!(i < args.Length-1 && int.TryParse(args[i+1], out var sleepMs)))
+                {
+                    sleepMs = 6000;
+                }
+                while (true)
+                {
+                    SetMonitorInState(MonitorState.MonitorStateOff);
+                    Thread.Sleep(sleepMs);
+                }
+            }
+            else
+            {
+                SetMonitorInState(MonitorState.MonitorStateOff);
+            }
         }
     }
 }
